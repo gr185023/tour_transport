bd42648e061c9530922ad5a39e89cae377e6223a
roven <11071319+gr185023@users.noreply.github.com>
2017-10-25 19:19:23 -0700

    google analyitcs

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ $_SESSION['tt']= 1;
 <!doctype html>
 <html class="no-js" lang="">
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108716875-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-108716875-1');
+        </script>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Fun Tour - Van and Car Rental</title>
@@ -583,15 +592,5 @@ $_SESSION['tt']= 1;
         <script src="js/vendor/materialize.min.js"></script>
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places,geometry&amp;key=AIzaSyCrjyb-LnK_jeqCM5-G8LWxvZ7IMiwoz1I"></script>
         <script src="js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
     </body>
 </html>
